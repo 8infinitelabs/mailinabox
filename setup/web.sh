d4e1d73cf8955e83c1403ddedbfcc9b378a27b8d
Diego Torres <diego@infinitelabs.co>
2021-07-14 13:33:31 +0200

    change place 7.3

--- setup/web.sh
+++ setup/web.sh
@@ -19,7 +19,7 @@ fi
 
 echo "Installing Nginx (web server)..."
 
-apt_install nginx php-cli php-fpm idn2
+apt_install nginx php7.3-cli php7.3-fpm idn2
 
 rm -f /etc/nginx/sites-enabled/default
 
--- setup/zpush.sh
+++ setup/zpush.sh
@@ -17,7 +17,7 @@ source /etc/mailinabox.conf # load global vars
 
 echo "Installing Z-Push (Exchange/ActiveSync server)..."
 apt_install \
-	php-soap php-imap libawl-php php-xsl
+	php7.3-soap php7.3-imap libawl-php php7.3-xsl
 
 phpenmod -v php imap
 
